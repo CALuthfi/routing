3e31cd16eb2795a3fb1858028004442b31179b3b
CALuthfi <choiriahmadluthfi@gmail.com>
2019-04-09 14:37:25 +0700

    Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
+// import ProductDetails from "./components/productDetails";
+// import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
@@ -15,10 +15,12 @@ class App extends Component {
       <div>
         <NavBar />
         <div path="contennt">
-          <Route path="/products" component={Products} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" exact component={Home} />
+          </Switch>
         </div>
       </div>
     );
